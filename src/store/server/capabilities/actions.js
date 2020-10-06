ad3fad0d081ed88618a306b178eee08392186135
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2020-10-06 19:33:37 +0200

    refactor: change load server capabilities action

--- src/store/server/capabilities/actions.js
+++ src/store/server/capabilities/actions.js
@@ -1,5 +1,5 @@
-import { Either, Left } from 'monet'
-import { keys, forEach, partial } from 'ramda'
+import { Left } from 'monet'
+import { keys, forEach } from 'ramda'
 import { Services } from '@/common/constants'
 import { CapabilitiesMutations } from '@/common/constants/mutations'
 import { getCapabilitiesOperationRequestConfig } from '@/modules/server/service/operation'
@@ -12,78 +12,61 @@ import { parseXml } from '@/modules/server/service/capabilities'
  * @param {ServerConfigObject} server
  * @param {Services} service
  */
-const fetchServerServiceCapabilities = ({ dispatch }, { server, service }) => {
-  return dispatch('client/fetch', getCapabilitiesOperationRequestConfig(server, service), { root: true })
+const fetchServerServiceCapabilities = async ({ dispatch }, { server, service }) => {
+  return await dispatch('client/fetch', getCapabilitiesOperationRequestConfig(server, service), { root: true })
+}
+
+const loadServiceCapabilities = async ({ dispatch, commit }, { server, service }) => {
+  /**
+   * @param {string} xml
+   * @return {string}
+   */
+  const commitXml = xml => {
+    commit(CapabilitiesMutations.SET_CAPABILITIES_XML, {
+      name: server.name,
+      service,
+      xml
+    })
+    return xml
+  }
+  /**
+   * @param {Object} parsed
+   * @return {Object}
+   */
+  const commitParsed = parsed => {
+    commit(CapabilitiesMutations.SET_CAPABILITIES_PARSED, {
+      name: server.name,
+      service,
+      parsed
+    })
+    return parsed
+  }
+  const commitLeft = error => {
+    const left = Left(error)
+    commit(CapabilitiesMutations.SET_CAPABILITIES_PARSED, {
+      name: server.name,
+      service,
+      parsed: left
+    })
+    return left
+  }
+  const capabilities = await fetchServerServiceCapabilities({ dispatch }, { server, service })
+  return capabilities
+    .map(commitXml).catchMap(commitLeft)
+    .map(parseXml)
+    .map(commitParsed).catchMap(commitLeft)
 }
 
 export default {
-
-  /**
-   * @param dispatch
-   * @param commit
-   * @param {ServerConfigObject} server
-   */
-  loadServerCapabilities ({ dispatch, commit }, { server }) {
+  async loadServicesCapabilities ({ dispatch, commit }, server) {
     commit(CapabilitiesMutations.SET_SERVER_CAPABILITIES, server.name)
-    const commitLeft = (service, error) => {
-      const left = Left(error)
-      commit(CapabilitiesMutations.SET_CAPABILITIES_PARSED, {
-        name: server.name,
-        service,
-        parsed: left
-      })
-      return left
-    }
-    /**
-     * @param {Services} service
-     * @param {string} xml
-     * @return {string}
-     */
-    const commitXml = (service, xml) => {
-      commit(CapabilitiesMutations.SET_CAPABILITIES_XML, {
-        name: server.name,
-        service,
-        xml
-      })
-      return xml
-    }
-    /**
-     * @param {Services} service
-     * @param {Object} parsed
-     * @return {void}
-     */
-    const commitParsed = (service, parsed) => {
-      commit(CapabilitiesMutations.SET_CAPABILITIES_PARSED, {
-        name: server.name,
-        service,
-        parsed
-      })
-    }
-
-    /**
-     * Fetch service capabilities, parses XML and commit both XML and parsed
-     * @param {Services }service
-     */
-    const fetchCapabilities = service => {
-      const loadCapabilities = xml => {
-        commitXml(service, xml)
-        Either.fromTry(() => parseXml(xml)).map(partial(commitParsed, [service]))
-      }
-      fetchServerServiceCapabilities(
-        { dispatch }, {
-          server,
-          service
-        }).then(
-        response => response.map(
-          loadCapabilities
-        ).catchMap(
-          partial(commitLeft, [service])
-        )
+    await Promise.all(
+      keys(Services).map(
+        service => loadServiceCapabilities({ dispatch, commit }, { server, service })
       )
-    }
-    forEach(fetchCapabilities, keys(Services))
+    )
   },
   loadServersCapabilities ({ dispatch, rootState }) {
-    forEach(server => dispatch('loadServerCapabilities', { server }), rootState.config.valid.servers)
+    forEach(server => dispatch('loadServicesCapabilities', server), rootState.config.valid.servers)
   }
 }
