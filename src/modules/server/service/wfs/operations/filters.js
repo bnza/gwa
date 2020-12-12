42362c826519967b42f5d4eba9e59059dbbb6812
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2020-12-12 16:49:12 +0100

    feat: add text contains filter

--- src/modules/server/service/wfs/operations/filters.js
+++ src/modules/server/service/wfs/operations/filters.js
@@ -2,7 +2,26 @@ import * as olFilters from 'ol/format/filter'
 import { find, propEq, map, concat } from 'ramda'
 import { not as notFilter, and as andFilter } from 'ol/format/filter'
 
+const LIKE_WILDCARD = '%'
+const LIKE_SINGLE_CHAR = '.'
+const LIKE_ESCAPE_CHAR = '!'
+const CASE_SENSITIVE = true
+const CASE_INSENSITIVE = false
+
 export const operators = [
+  {
+    text: 'text contains',
+    filter: olFilters.like,
+    types: ['string'],
+    cardinality: 1,
+    tagName: 'PropertyIsLike',
+    options: [LIKE_WILDCARD, LIKE_SINGLE_CHAR, LIKE_ESCAPE_CHAR, CASE_INSENSITIVE],
+    parseExpressions: filter => {
+      // TODO escape special chars
+      return filter.length ? [`${LIKE_WILDCARD}${filter[0]}${LIKE_WILDCARD}`] : undefined
+    },
+    validate: filter => !!filter.propertyName && !!filter.expressions.length
+  },
   {
     text: 'is null',
     filter: olFilters.isNull,
@@ -11,7 +30,6 @@ export const operators = [
     tagName: 'PropertyIsNull',
     options: [],
     parseExpressions: filter => [],
-    parseOptions: filter => undefined,
     validate: filter => !!filter.propertyName
   },
   {
@@ -20,9 +38,8 @@ export const operators = [
     types: ['string', 'number'],
     cardinality: 1,
     tagName: 'PropertyIsEqualTo',
-    options: [false], // matchCase
-    parseExpressions: filter => [filter.expression],
-    parseOptions: filter => [filter.matchCase],
+    options: [CASE_SENSITIVE],
+    parseExpressions: filter => filter,
     validate: filter => !!filter.propertyName && !!filter.expressions.length
   }
 ]
@@ -30,9 +47,12 @@ export const operators = [
 export const findByTagName = tagName => find(propEq('tagName', tagName))(operators)
 
 export const generateFilter = filterObject => {
+  const expressions = filterObject.operator.parseExpressions(
+    filterObject.expressions.slice(0, filterObject.operator.cardinality)
+  )
   let filter = filterObject.operator.filter(
     filterObject.propertyName.just(),
-    ...filterObject.expressions.slice(0, filterObject.operator.cardinality),
+    ...expressions,
     ...filterObject.operator.options
   )
   if (filterObject.negate) {
