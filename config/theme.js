ba0cee8b4f343c69a6765136e90c876a92978d16
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2020-12-12 15:20:12 +0100

    style: add Vuetify themes support

---
+++ config/theme.js
@@ -0,0 +1,10 @@
+export default {
+  primary: '#3f51b5',
+  secondary: '#2196f3',
+  accent: '#ffc107',
+  error: '#f44336',
+  warning: '#ff9800',
+  info: '#03a9f4',
+  success: '#4caf50',
+  background: '#fafafa'
+}
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import theme from '../../config/theme'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: theme
+    }
+  }
+})
