a92b8dc965019e473ad30d2c63ca045eb9c9fa02
pietro.baldassarri <pietro.baldassarri@gmail.com>
2021-09-12 09:20:48 +0200

    refactor: move layers config loader progressbar to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,6 +2,16 @@
   <v-footer>
     <div><p><span><v-icon>tonality</v-icon></span>{{config.dataProjection}}</p></div>
     <v-spacer></v-spacer>
+      <v-progress-linear
+        v-if="layersLoading"
+        fixed
+        striped
+        :value="percent"
+        height="20"
+        style="margin-left: 200px; display: inline; float: right; max-width: 200px"
+      >
+        Loading layers:<strong> {{this.loaded}}/{{this.total}} </strong>
+    </v-progress-linear>
     <div><p>{{cursorCoords[0].toFixed(6)}}, {{cursorCoords[1].toFixed(6)}}</p></div>
     <div><p>&copy; {{ new Date().getFullYear() }}</p></div>
   </v-footer>
@@ -11,9 +21,25 @@
 import { mapState } from 'vuex'
 export default {
   name: 'AppFooter',
+  props: {
+    layersLoading: {
+      type: Boolean,
+      required: true
+    }
+  },
   computed: {
     ...mapState('config', { config: 'valid' }),
-    ...mapState('components/AppMap', { cursorCoords: 'coords' })
+    ...mapState('components/AppMap', { cursorCoords: 'coords' }),
+    ...mapState('layers', {
+      layers: 'states',
+      loaded: 'loaded'
+    }),
+    total: function () {
+      return Object.keys(this.layers).length
+    },
+    percent: function () {
+      return Math.round((this.loaded / this.total + Number.EPSILON) * 100)
+    }
   }
 }
 </script>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,12 +2,11 @@
   <div>
     <app-config-manager @layersSet="layersSet=true" :layersLoading.sync="layersLoading"/>
     <app-bar />
-    <app-layer-loader-dialog :value="layersLoading" />
     <app-navigation-drawer :layers-set="layersSet"/>
       <v-main>
-        <app-map :layersReady="layersReady"/>
+        <app-map :layers-ready="layersReady"/>
       </v-main>
-    <app-footer />
+    <app-footer :layers-loading="layersLoading"/>
   </div>
 </template>
 
@@ -15,13 +14,12 @@
 import AppConfigManager from '@/components/AppConfigManager'
 import AppBar from '@/components/AppBar'
 import AppFooter from '@/components/AppFooter'
-import AppLayerLoaderDialog from '@/components/AppLayerLoaderDialog'
 import AppMap from '@/components/AppMap'
 import AppNavigationDrawer from '@/components/AppNavigationDrawer'
 
 export default {
   name: 'AppMain',
-  components: { AppBar, AppConfigManager, AppFooter, AppMap, AppNavigationDrawer, AppLayerLoaderDialog },
+  components: { AppBar, AppConfigManager, AppFooter, AppMap, AppNavigationDrawer },
   data () {
     return {
       layersSet: false,
